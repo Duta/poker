35e844b79bfbb90860efba423fa03f2c289f43e0
Bertie Wheen <wheen.b@gmail.com>
2014-12-14 04:24:28 +0000

    Foundations of getting a hand type

--- haskell/Poker.hs
+++ haskell/Poker.hs
@@ -28,7 +28,7 @@ data Card = Card
   , _suit :: Suit
   } deriving (Show, Eq, Ord)
 
-type Hand = (Card, Card)
+type HoleCards = (Card, Card)
 
 data CommunityCards
   = Preflop
@@ -40,13 +40,47 @@ data CommunityCards
 type ChipCount = Integer
 
 data Player = Player
-  { _holeCards :: Maybe Hand
+  { _holeCards :: Maybe HoleCards
   , _chips :: ChipCount
   } deriving Show
 
 data Game = Game
   { _players :: [Player]
-  , _communityCards :: CommunityCards
+  , _commCards :: CommunityCards
   } deriving Show
 
 type Deck = [Card]
+
+data Action
+  = Fold
+  | Call ChipCount -- ChipCount actually required?
+  | Raise ChipCount
+    deriving Show
+
+data HandType
+  = HighCard      Rank
+  | OnePair       Rank
+  | TwoPair       Rank Rank -- First rank is the higher
+  | ThreeOfAKind  Rank
+  | Straight      Rank      -- Highest rank of the straight
+  | Flush         Rank      -- Highest rank of the flush
+  | FullHouse     Rank Rank -- First rank is the triple
+  | Quads         Rank Rank -- First rank is that of the quads, second is the kicker
+  | StraightFlush Rank      -- Highest rank of the straight flush
+  | RoyalFlush
+    deriving (Show, Eq, Ord)
+
+getType :: CommunityCards -> HoleCards -> HandType
+getType commCards holeCards = getType' cards
+ where
+  mergeIntoList :: CommunityCards -> HoleCards -> [Card]
+  mergeIntoList  Preflop                    (hc1, hc2) = [                         hc1, hc2]
+  mergeIntoList (Flop  cc1 cc2 cc3)         (hc1, hc2) = [cc1, cc2, cc3,           hc1, hc2]
+  mergeIntoList (Turn  cc1 cc2 cc3 cc4)     (hc1, hc2) = [cc1, cc2, cc3, cc4,      hc1, hc2]
+  mergeIntoList (River cc1 cc2 cc3 cc4 cc5) (hc1, hc2) = [cc1, cc2, cc3, cc4, cc5, hc1, hc2]
+
+  cards :: [Card]
+  cards = mergeIntoList commCards holeCards
+
+  getType' :: [Card] -> HandType
+  getType' cs = _ht
